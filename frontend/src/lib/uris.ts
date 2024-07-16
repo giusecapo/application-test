646c44b26e7f3d95866ff7a111e3915741fb11a1
Giuseppe Capoluongo <giuse_capo@icloud.com>
2024-07-16 16:27:36 +0200

    Some comments

--- frontend/src/lib/uris.ts
+++ frontend/src/lib/uris.ts
@@ -1,3 +1,4 @@
+// @giusecapo: need to check if uri is needed for client side or server side API call. If server-side, docker container name is the correct url. Dev only.
 export const isServer = () => typeof window === `undefined`;
 
 const defaultUrl: string = process.env.NEXT_PUBLIC_DEFAULT_URL ?? "";
